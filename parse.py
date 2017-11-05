9e94fd3c44aac520ea8252e4a28633d2ec80434f
atsmin <atsmin@users.noreply.github.com>
2017-11-05 17:00:06 +0900

    first commit

---
+++ parse.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import re
+
+DIR = './lyrics'
+
+with open('./urls.txt', 'r') as f:
+    for url in f:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+
+        title = soup.find_all('h2')[0].text.encode('utf-8')
+        title = re.sub(r'^\'', '', title)
+        title = re.sub(r'^\"', '', title)
+        title = re.sub(r'\'$', '', title)
+        title = re.sub(r'\"$', '', title)
+        title = title.replace(' 歌詞', '').strip()
+        print title
+
+        lyric = '\n'.join([x.encode('utf-8') for x in soup.find(id='Lyric').stripped_strings])
+        lyric = lyric.replace('<br/>', '。').replace('(※くり返し)', '').replace('(△くり返し)', '').replace('※', '').replace('△', '')
+        lyric = lyric.strip()
+        with open(DIR + '/' + title, 'w') as f2:
+            f2.write(lyric)
+
