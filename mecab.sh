e03d7a73fc51e13288c4242ba7082aa76863994f
atsmin <atsmin@users.noreply.github.com>
2017-11-13 06:52:33 +0900

    mod

--- mecab.sh
+++ mecab.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 mecab lyric.txt -o mecab.txt
-cat  mecab.txt | grep -v EOS | grep -v 助動詞 | grep -v 助詞 | grep -v 記号 | grep -v 非自立 | grep -v サ変 | grep -v 接尾 | grep -v 特殊 | cut -f 1 | sort | uniq -c | sort > result.txt
-
+cat mecab.txt | egrep -v 'EOS|助動詞|助詞|記号|非自立|サ変|接尾|特殊' | cut -f 1 | sort | uniq -c | sort | awk 'BEGIN{print "id, value"}{print $2", "$1}' > result.csv
