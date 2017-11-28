189145ef9699a08249ddb738a37bf66ef895a671
atsmin <atsmin@users.noreply.github.com>
2017-11-29 06:40:46 +0900

    change to python3

--- lyric.py
+++ lyric.py
@@ -10,15 +10,15 @@ with open('./urls.txt', 'r') as f:
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'html.parser')
 
-        title = soup.find_all('h2')[0].text.encode('utf-8')
-        title = re.sub(r'^\'', '', title)
-        title = re.sub(r'^\"', '', title)
-        title = re.sub(r'\'$', '', title)
-        title = re.sub(r'\"$', '', title)
+        title = soup.find_all('h2')[0].text
+        title = re.sub('^\'', '', title)
+        title = re.sub('^\"', '', title)
+        title = re.sub('\'$', '', title)
+        title = re.sub('\"$', '', title)
         title = title.replace(' 歌詞', '').strip()
-        print title
+        print(title)
 
-        lyric = '\n'.join([x.encode('utf-8') for x in soup.find(id='Lyric').stripped_strings if len(x) > 0])
+        lyric = '\n'.join([x for x in soup.find(id='Lyric').stripped_strings if len(x) > 0])
         lyric = lyric.replace('<br/>', '。').replace('(※くり返し)', '').replace('(△くり返し)', '').replace('※', '').replace('△', '')
         lyric = lyric.strip()
         with open(DIR + '/' + title, 'w') as f2:
